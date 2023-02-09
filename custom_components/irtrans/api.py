e6e8393a7d17574b8263bbdd6a4548266c775f8c
Frank Schwarzenberg <52888262+schwarzenbergf@users.noreply.github.com>
2023-02-09 17:17:33 +0100

    Update api.py

--- custom_components/irtrans/api.py
+++ custom_components/irtrans/api.py
@@ -39,6 +39,7 @@ class IRTransCon(asyncio.Protocol):
     @staticmethod
     async def write_data(transport, data):
         """Write Data to socket"""
+
         try:
             transport.write(data.encode())
             while transport.get_write_buffer_size() > 0:
@@ -93,8 +94,8 @@ class IRTransCon(asyncio.Protocol):
         if DEBUG:
             _LOGGER.debug("The server closed the connection %s", exc)
         self.mycfg["irtrans"] = "disconnected"
-        self.trans_port.close()
-        self.trans_port = None
+        IRTransCon.trans_port.close()
+        IRTransCon.trans_port = None
         self.on_con_lost.set_result(True)
 
 
